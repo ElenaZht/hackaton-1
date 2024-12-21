7863f3dc07f5c74e93f5554f4abd6bd979552edc
Elena Zht <elenazht@gmail.com>
2024-12-21 23:02:31 +0200

    - integrate task functionality; - add task menu

--- list.py
+++ list.py
@@ -1,3 +1,6 @@
+from task_menu import load_tasks
+
+
 class ToDoList:
     tasks = []
 
@@ -6,15 +9,12 @@ class ToDoList:
         self.list_id = list_id
         
 
-    def show_list(self):
+    def show_list(self, conn):
         '''displays all list tasks'''
+        self.tasks = load_tasks(self.list_id, conn)
 
-        print(f'List {self.list_id} - {self.list_name}')
-        for task in self.tasks:
-            print(f'*{task.id} -{task.text} {task.priority} -{task.estimation}')
-        
-        self.show_progress()
-    
+        print(f'List {self.list_id} - {self.list_name} {len(self.tasks)}')
+            
     def save(self, conn):
         query = f"INSERT INTO list (name) VALUES ('{self.list_name}') returning *"
         with  conn.cursor() as cur:
@@ -23,7 +23,7 @@ class ToDoList:
             # get the generated id back
             row = cur.fetchone()
             if row:
-                self.list_id = row[1]
+                self.list_id = row[0]
                 print(f'list {self.list_name} added successfuly')
             conn.commit()
 
@@ -69,64 +69,6 @@ class ToDoList:
                 if task.is_done:
                     completed += 1
             progres = completed / len(self.tasks) * 100
+            print(f"{progres}% is ready")
         #todo: call draw_progress(progres)
         
-
-def change_list_name(todo_list, conn):
-    new_name = input('enter new name: ')
-    todo_list.update_list_name(new_name, conn)
-
-def add_new_list(connection):
-    list_name = input('enter list name: ')
-    new_list = ToDoList(list_name)
-    new_list.save(connection)
-
-def init_db(conn):
-    query = '''CREATE TABLE IF NOT EXISTS list (
-    name VARCHAR(100) NOT NULL,
-    list_id SERIAL,
-    user_id INT);'''
-    with  conn.cursor() as cur:
-        cur.execute(query)
-        conn.commit()
-
-def show_all_lists(conn):
-    todos_dict = {}
-    query = 'SELECT * FROM list'
-    with  conn.cursor() as cur:
-        cur.execute(query)
-        rows = cur.fetchall()
-
-        if rows:
-            for row in rows:
-                l = ToDoList(row[0], row[1])
-                l.show_list()
-                todos_dict[l.list_id] = l
-        conn.commit()
-    return todos_dict   
-    
-#list menu
-def show_list_menu(todo_list, conn):
-    option = ''
-    while True:
-        print('''
-            * (u) update list
-            * (d) delete list
-            * (s) show list
-            * (x) exit
-            ''')
-        option = input('chose option: ')
-        if option == 'u':
-            change_list_name(todo_list, conn)
-        
-        if option == 'd':
-            todo_list.delete_list(conn)
-            return
-
-        if option == 's':
-            todo_list.show_list()
-        
-        if option == 'x':
-            return
-        else:
-            print('chose valid option')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from list import show_list_menu, add_new_list, show_all_lists, init_db
+from list_menu import show_list_menu, add_new_list, show_all_lists, create_list_table
 import psycopg2
+from task_menu import create_task_table
 
 
 def show_main_menu(conn):
@@ -11,35 +12,37 @@ def show_main_menu(conn):
             * (l) my lists
             * (x) exit \n''')
         
-        option = input('chose option: ')
+        option = input('choose option: ')
         if option == 'a':
             add_new_list(conn)
 
         elif option == 'l':
             print('** My lists **')
             lists_dict = show_all_lists(conn)
-            list_id = int(input('enter list id or (x) exit: '))
-            if list_id == 'x':
-                return
-            show_list_menu(lists_dict[list_id], conn)
-
+            while option != 'x':
+                list_id = input('enter list id or (x) exit: ')
+                if list_id == 'x':
+                    break
+                show_list_menu(lists_dict[int(list_id)], conn)
+                
         elif option == 'x':
             return
         
         else:
-            print('chose valid option')
+            print('choose valid option')
         
 
 
 def main():
-    conn = psycopg2.connect(
+    with  psycopg2.connect(
             dbname="todo",  # your DB name
             user="postgres",      # your username
             password="lena_postgres",  # your password
             host="localhost"      # your host (localhost for local machine)
-        )
-    init_db(conn)
-    show_main_menu(conn)
+        ) as conn:
+        create_list_table(conn)
+        create_task_table(conn)
+        show_main_menu(conn)
 
 
 
